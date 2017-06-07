e3df6f84f42619a67a2eefaed0336e0f9bb59cc8
Jan Nils Ferner <jn_ferner@hotmail.de>
2017-06-07 23:05:52 +0200

    Kill me

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,82 +1,59 @@
 extern crate websocket;
 extern crate futures;
 extern crate tokio_core;
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
 
 use std::fmt::Debug;
 
 use websocket::message::{Message, OwnedMessage};
 use websocket::server::InvalidConnection;
-use websocket::async::Server;
+use websocket::client::async::Framed;
+use websocket::async::{Server, MessageCodec};
+use websocket::WebSocketError;
 
+use tokio_core::net::TcpStream;
 use tokio_core::reactor::{Handle, Core};
+
 use futures::{Future, Sink, Stream};
 use futures::future::{self, Loop};
+use std::sync::{Mutex, Arc};
 
 fn main() {
     let mut core = Core::new().unwrap();
     let handle = core.handle();
-    // bind to the server
     let server = Server::bind("localhost:8081", &handle).unwrap();
 
-    // time to build the server's future
-    // this will be a struct containing everything the server is going to do
-
-    // a stream of incoming connections
     let f = server.incoming()
         // we don't wanna save the stream if it drops
         .map_err(|InvalidConnection { error, .. }| error)
         .for_each(|(upgrade, addr)| {
             println!("Got a connection from: {}", addr);
-            // check if it has the protocol we want
             if !upgrade.protocols().iter().any(|s| s == "rust-websocket") {
-                // reject it if it doesn't
                 spawn_future(upgrade.reject(), "Upgrade Rejection", &handle);
                 return Ok(());
             }
 
-            // accept the request to be a ws connection if it does
             let f = upgrade
                 .use_protocol("rust-websocket")
                 .accept()
-                // send a greeting!
                 .and_then(|(server, _)| server.send(Message::text("Hello World!").into()))
-                // simple echo server impl
-                .and_then(move |server| {
+                .and_then(|server| {
                     future::loop_fn(server, |stream| {
                         stream.into_future()
                             .or_else(|(err, stream)| {
                                 println!("Could not send message: {:?}", err);
                                 stream.send(OwnedMessage::Close(None)).map(|s| (None, s))
                             })
-                            .and_then(|(msg, stream)| match msg {
-                                Some(OwnedMessage::Text(txt)) => {
-                                    stream.send(OwnedMessage::Text(txt))
-                                        .map(|s| Loop::Continue(s))
-                                        .boxed()
-                                }
-                                Some(OwnedMessage::Binary(bin)) => {
-                                    stream.send(OwnedMessage::Binary(bin))
-                                        .map(|s| Loop::Continue(s))
-                                        .boxed()
-                                }
-                                Some(OwnedMessage::Ping(data)) => {
-                                    stream.send(OwnedMessage::Pong(data))
-                                        .map(|s| Loop::Continue(s))
-                                        .boxed()
-                                }
-                                Some(OwnedMessage::Close(_)) => {
-                                    stream.send(OwnedMessage::Close(None))
-                                        .map(|_| Loop::Break(()))
-                                        .boxed()
-                                }
-                                Some(OwnedMessage::Pong(_)) => {
-                                    future::ok(Loop::Continue(stream)).boxed()
-                                }
-                                None => future::ok(Loop::Break(())).boxed(),
+                            .and_then(|(msg, stream)|{
+                                let mut state = State::new();
+                                handle_incomming(&mut state, msg);
+                                send(&state, stream)
                             })
                     })
                 });
-
             spawn_future(f, "Client Status", &handle);
             Ok(())
         });
@@ -93,4 +70,68 @@ fn spawn_future<F, I, E>(f: F, desc: &'static str, handle: &Handle)
         .map(move |_| println!("{}: Finished.", desc)));
 }
 
+
+fn handle_incomming(state: &mut State, msg: Option<OwnedMessage>) {
+    /*match msg {
+        Some(OwnedMessage::Text(txt)) => {
+            stream.send(OwnedMessage::Text(txt))
+                .map(|s| Loop::Continue(s))
+                .boxed()
+        }
+        Some(OwnedMessage::Binary(bin)) => {
+            stream.send(OwnedMessage::Binary(bin))
+                .map(|s| Loop::Continue(s))
+                .boxed()
+        }
+        Some(OwnedMessage::Ping(data)) => {
+            stream.send(OwnedMessage::Pong(data))
+                .map(|s| Loop::Continue(s))
+                .boxed()
+        }
+        Some(OwnedMessage::Close(_)) => {
+            stream.send(OwnedMessage::Close(None))
+                .map(|_| Loop::Break(()))
+                .boxed()
+        }
+        Some(OwnedMessage::Pong(_)) => {
+            future::ok(Loop::Continue(stream)).boxed()
+        }
+        None => {
+            future::ok(Loop::Break(())).boxed()
+        },
+    }*/
+}
+
+type FramedStream = Framed<TcpStream, MessageCodec<OwnedMessage>>;
+
+fn send(state: &State, stream: FramedStream) -> Box<Future<Item=Loop<(), FramedStream>,Error=WebSocketError>> {
+    stream
+        .send(OwnedMessage::Text(serde_json::to_string(&state).unwrap()))
+        .map(|s| {
+            if shutdown() {
+                Loop::Break(())
+            } else {
+                Loop::Continue(s)
+            }
+        })
+        .boxed()
+
+}
+
+fn shutdown() -> bool {
+    false
+}
+
+#[derive(Serialize)]
+struct State {
+    msg: String,
+}
+
+impl State {
+    fn new() -> State {
+        State {
+            msg: "git gud".to_string()
+        }
+    }
+}
 
