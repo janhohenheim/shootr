6acbf4ed75226713b3f0e24618079cd218e20c6b
Jan Nils Ferner <jn_ferner@hotmail.de>
2017-07-09 16:12:35 +0200

    Core: Change config from ms/update to updates/sec
    This should be more intuitive to tweak

--- core/src/main.rs
+++ core/src/main.rs
@@ -38,7 +38,7 @@ impl EventHandler for Handler {
         for _ in 0..1 {
             world
                 .create_entity()
-                .with(comp::Vel { x: 3, y: 2 })
+                .with(comp::Vel { x: 8, y: 6 })
                 .with(comp::Pos { x: 500, y: 500 })
                 .build();
         }
@@ -52,10 +52,10 @@ impl EventHandler for Handler {
 
         let mut lag: u64 = 0;
         let mut previous = Utc::now();
-        let ms_per_update = read_env_var("CORE_MS_PER_UPDATE").parse::<u64>().expect(
+        let updates_per_sec = read_env_var("CORE_UPDATES_PER_SEC").parse::<u64>().expect(
             "Failed to parse environmental variable as integer",
         );
-
+        let ms_per_update = 1000 / updates_per_sec;
         loop {
             let current = Utc::now();
             let elapsed = elapsed_time(previous, current);
