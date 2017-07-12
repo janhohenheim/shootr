0b65b1272793fe45b7d75f97a78ddf529649c55e
Jan Nils Ferner <jn_ferner@hotmail.de>
2017-07-12 21:24:47 +0200

    Client: Lower interpolation time

--- web/public/index.js
+++ web/public/index.js
@@ -161,7 +161,7 @@ function render(states) {
 
 function getRenderTime() {
     const now = new Date().getTime()
-    const INTERPOLATION_DELTA = 300
+    const INTERPOLATION_DELTA = 200
     return now - INTERPOLATION_DELTA
 }
 
