33783ed9724a0d390c6f2ecd6639ffc09fd70731
Jan Nils Ferner <jn_ferner@hotmail.de>
2017-07-14 18:44:30 +0200

    Client: Display connecting and disconnecting players

--- web/public/index.js
+++ web/public/index.js
@@ -32,6 +32,14 @@ function connect(address) {
 
     io.onmessage = (msg) => {
         states.push(JSON.parse(msg.data, (key, value) => value === "" ? 0 : value))
+        const state = states[states.length - 1]
+        while (players.length < state.players.length) {
+            spawnPlayer()
+        }
+
+        while (players.length > state.players.length) {
+            removePlayer()
+        }
     }
 
     io.onclose = () => {
@@ -126,7 +134,7 @@ function loadProgressHandler(loader, resource) {
 }
 
 let ball
-let player
+let players = []
 
 function setup() {
     const background = new Sprite(resources.pong.textures['fancy-court.png'])
@@ -145,11 +153,6 @@ function setup() {
     ball = new Sprite(resources.pong.textures['fancy-ball.png'])
     ball.anchor.set(0.5)
     app.stage.addChild(ball)
-
-    player = new Sprite(resources.pong.textures['fancy-paddle-green.png'])
-    player.anchor.set(0.5)
-    app.stage.addChild(player)
-
 
     app.ticker.add(gameLoop)
 }
@@ -211,12 +214,16 @@ function getInterpolatedState(from, to, renderTime) {
     state.ball.pos.x += (to.ball.pos.x - from.ball.pos.x) * fraction
     state.ball.pos.y += (to.ball.pos.y - from.ball.pos.y) * fraction
 
-    state.player.acc.x += (to.player.acc.x - from.player.acc.x) * fraction
-    state.player.acc.y += (to.player.acc.y - from.player.acc.y) * fraction
-    state.player.vel.x += (to.player.vel.x - from.player.vel.x) * fraction
-    state.player.vel.y += (to.player.vel.y - from.player.vel.y) * fraction
-    state.player.pos.x += (to.player.pos.x - from.player.pos.x) * fraction
-    state.player.pos.y += (to.player.pos.y - from.player.pos.y) * fraction
+    for (let i = 0; i < players.length; i++) {
+        if (!to.players[i])
+            continue;
+        state.players[i].acc.x += (to.players[i].acc.x - from.players[i].acc.x) * fraction
+        state.players[i].acc.y += (to.players[i].acc.y - from.players[i].acc.y) * fraction
+        state.players[i].vel.x += (to.players[i].vel.x - from.players[i].vel.x) * fraction
+        state.players[i].vel.y += (to.players[i].vel.y - from.players[i].vel.y) * fraction
+        state.players[i].pos.x += (to.players[i].pos.x - from.players[i].pos.x) * fraction
+        state.players[i].pos.y += (to.players[i].pos.y - from.players[i].pos.y) * fraction
+    }
 
     state.timestamp = renderTime
     return state
@@ -227,8 +234,12 @@ function setWorld(state) {
     ball.x = state.ball.pos.x
     ball.y = state.ball.pos.y
 
-    player.x = state.player.pos.x
-    player.y = state.player.pos.y
+    for (let i = 0; i < players.length; i++) {
+        if (!state.players[i])
+            continue;
+        players[i].x = state.players[i].pos.x
+        players[i].y = state.players[i].pos.y
+    }    
 
     const maxVel = Math.max(state.ball.vel.x, state.ball.vel.y)
     const strength = Math.pow(Math.atan(Math.pow((maxVel / 10), 1.5)), 2) - 0.2
@@ -236,4 +247,17 @@ function setWorld(state) {
         const blurFilter = new PIXI.filters.BlurFilter(strength, 1, 1)
         ball.filters = [blurFilter]
     }
+}
+
+function spawnPlayer() {
+    const player = new Sprite(resources.pong.textures['fancy-paddle-green.png'])
+    player.anchor.set(0.5)
+    app.stage.addChild(player)
+    players.push(player)
+}
+
+function removePlayer() {
+    const player = players[players.length - 1]
+    app.stage.removeChild(player)
+    players.pop()
 }
