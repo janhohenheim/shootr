67459d196006024f5dde577d446cf71855c2cc41
Jan Nils Ferner <jn_ferner@hotmail.de>
2017-07-23 20:50:49 +0200

    Core: Run rustfmt

--- core/src/system/sending.rs
+++ core/src/system/sending.rs
@@ -58,25 +58,28 @@ where
 }
 
 
-fn handle_new_connections(player: &ReadStorage<PlayerComp>, entities: &EntitiesRes, actor: &ReadStorage<Actor>, connect: &mut WriteStorage<Connect>) {
+fn handle_new_connections(
+    player: &ReadStorage<PlayerComp>,
+    entities: &EntitiesRes,
+    actor: &ReadStorage<Actor>,
+    connect: &mut WriteStorage<Connect>,
+) {
     let mut new_connections = Vec::new();
-    for (new_player, entity, new_actor, _) in
-        (player, entities, actor, &mut *connect).join()
-        {
-            new_connections.push((entity.clone(), new_actor.clone()));
-            let mut payload = Vec::new();
-            payload.push(serialize(&new_actor.id));
-            let mut id_actor_kind = HashMap::new();
-            for actor in (&actor).join() {
-                id_actor_kind.insert(actor.id, actor.kind.clone());
-            }
-            payload.push(serialize(&id_actor_kind));
-            let greeting = ClientMessage {
-                opcode: OpCode::Greeting,
-                payload: payload,
-            };
-            send(new_player, &greeting);
+    for (new_player, entity, new_actor, _) in (player, entities, actor, &mut *connect).join() {
+        new_connections.push((entity.clone(), new_actor.clone()));
+        let mut payload = Vec::new();
+        payload.push(serialize(&new_actor.id));
+        let mut id_actor_kind = HashMap::new();
+        for actor in (&actor).join() {
+            id_actor_kind.insert(actor.id, actor.kind.clone());
         }
+        payload.push(serialize(&id_actor_kind));
+        let greeting = ClientMessage {
+            opcode: OpCode::Greeting,
+            payload: payload,
+        };
+        send(new_player, &greeting);
+    }
 
     for new_connection in new_connections {
         let (new_entity, new_actor) = new_connection;
@@ -93,7 +96,11 @@ fn handle_new_connections(player: &ReadStorage<PlayerComp>, entities: &EntitiesR
     }
 }
 
-fn handle_disconnects(player: &ReadStorage<PlayerComp>, actor: &ReadStorage<Actor>, disconnect: &ReadStorage<Disconnect>) {
+fn handle_disconnects(
+    player: &ReadStorage<PlayerComp>,
+    actor: &ReadStorage<Actor>,
+    disconnect: &ReadStorage<Disconnect>,
+) {
     for (actor, _) in (actor, disconnect).join() {
         let disconnect = ClientMessage {
             opcode: OpCode::Disconnect,
@@ -106,7 +113,12 @@ fn handle_disconnects(player: &ReadStorage<PlayerComp>, actor: &ReadStorage<Acto
 
 }
 
-fn send_world_updates(player: &ReadStorage<PlayerComp>, actor: &ReadStorage<Actor>, pos: &ReadStorage<Pos>, vel: &ReadStorage<Vel>) {
+fn send_world_updates(
+    player: &ReadStorage<PlayerComp>,
+    actor: &ReadStorage<Actor>,
+    pos: &ReadStorage<Pos>,
+    vel: &ReadStorage<Vel>,
+) {
     let mut serialized_actors = HashMap::new();
     for actor in (actor).join() {
         serialized_actors.insert(actor.id, HashMap::new());
