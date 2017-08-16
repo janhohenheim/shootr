06cf06158b6ac96e7e156a1e1945c66711941a69
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-08-16 21:45:19 +0200

    Client: Make rollup work

--- web/client/rollup.config.js
+++ web/client/rollup.config.js
@@ -3,6 +3,7 @@ import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify-es'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+import builtins from 'rollup-plugin-node-builtins'
 
 export default {
   entry: './src/index.ts',
@@ -10,33 +11,55 @@ export default {
   format: 'iife',
   sourceMap: true,
   plugins: [
-      resolve({
-          // use "module" field for ES6 module if possible
-          module: true, // Default: true
-
-          // use "jsnext:main" if possible
-          // – see https://github.com/rollup/rollup/wiki/jsnext:main
-          jsnext: true,  // Default: false
-
-
-
-          // not all files you want to resolve are .js files
-          extensions: [ '.js', '.json', '.ts' ],  // Default: ['.js']
-
-          // whether to prefer built-in modules (e.g. `fs`, `path`) or
-          // local ones with the same names
-          preferBuiltins: false,  // Default: true
-
-
-          // If true, inspect resolved files to check that they are
-          // ES2015 modules
-          modulesOnly: true, // Default: false
-      }),
-      commonjs(),
     typescript({
       typescript: require('typescript')
     }),
-    babel(),
-    uglify()
+    babel({
+      babelrc: false,
+      comments: false,
+      presets: [
+        [
+          'env',
+          {
+            modules: false,
+            targets: {
+              browsers: [
+                'last 1 chrome version',
+                'last 1 firefox version',
+                'last 1 safari version',
+                'last 1 ios_saf version',
+                'last 1 opera version',
+                'last 1 edge version'
+              ]
+            }
+          },
+        ]
+      ],
+      plugins: [
+        'external-helpers'
+      ],
+    }),
+    resolve({
+      // use "module" field for ES6 module if possible
+      module: true, // Default: true
+
+      // use "jsnext:main" if possible
+      // – see https://github.com/rollup/rollup/wiki/jsnext:main
+      jsnext: true,  // Default: false
+
+      // not all files you want to resolve are .js files
+      extensions: ['.js', '.json', '.ts'],  // Default: ['.js']
+
+      // whether to prefer built-in modules (e.g. `fs`, `path`) or
+      // local ones with the same names
+      preferBuiltins: false,  // Default: true
+
+      // If true, inspect resolved files to check that they are
+      // ES2015 modules
+      modulesOnly: false, // Default: false
+    }),
+    commonjs(),
+    builtins(),
+    // uglify(),
   ]
 }
